0df52d9ef2abefc24a99d6a08c48937589678ea7
josafa <desenvolvedorjosafa@gmail.com>
2016-09-03 18:04:28 -0300

    layou payment customizado

--- app/models/client.rb
+++ app/models/client.rb
@@ -4,4 +4,31 @@ class Client < ActiveRecord::Base
     def selection_payment type
         self.payments.where payment_type: type
     end
+
+    def debit
+        value = value_group_client 0
+        Client.valide_value value
+    end
+
+    def paid
+        value = value_group_client 1
+        Client.valide_value value
+    end
+
+    def self.valide_value value
+        unless value.nil?
+            return value
+        end
+        return 0.0
+    end
+
+    def cashier
+        value = paid - debit
+    end
+
+    private
+    def value_group_client a
+        value = payments.group(:payment_type).sum(:price)
+        value[a]
+    end
 end
--- app/models/payment.rb
+++ app/models/payment.rb
@@ -4,4 +4,22 @@ class Payment < ActiveRecord::Base
     #validates :client, presence: true
 
     enum payment_type: [:debito,:pago]
+
+    def self.value_paid
+        Payment.value_group_payment 1
+    end
+
+    def self.value_debit
+        Payment.value_group_payment 0
+    end
+
+    def self.cashier
+        Payment.value_paid - Payment.value_debit
+    end
+
+    private
+    def self.value_group_payment a
+        value = Payment.group(:payment_type).sum(:price)
+        value[a]
+    end
 end
