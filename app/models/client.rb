c33e1e9e0a9bb28511c2d0f5e23f0f2b9eae741c
josafa <desenvolvedorjosafa@gmail.com>
2016-09-05 11:19:27 -0300

    client cascate add para check

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,6 +1,6 @@
 class Client < ActiveRecord::Base
     has_many :payments, dependent: :destroy
-    has_many :checks
+    has_many :checks, dependent: :destroy
 
     def selection_payment type
         self.payments.where payment_type: type
